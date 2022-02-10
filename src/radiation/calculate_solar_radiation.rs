c68ee46aadcd2653b59bae54b3d0ade137365a2e
Filip Pruzinec <pruzinec.f@gmail.com>
2022-02-10 12:00:16 +0100

    illumination map

--- src/radiation/calculate_solar_radiation.rs
+++ src/radiation/calculate_solar_radiation.rs
@@ -1,11 +1,11 @@
 use chrono::prelude::*;
 use chrono::Utc;
 use rayon::prelude::*;
-use std::collections::HashMap;
-use std::hash::BuildHasherDefault;
-use twox_hash::XxHash64;
+use std::rc::Rc;
 
-use super::illumination::get_rotated_voxel_key_pair_iterator;
+use super::illumination::{
+    get_rotated_voxel_key_pair_iterator, IlluminationMap, IlluminationMapUtils,
+};
 use super::radiation_components::get_irradiance;
 use super::radiation_components::VoxelIrradiance;
 use super::sun_position::get_sun_positions;
@@ -24,54 +24,21 @@ pub fn calculate_solar_radiation(voxel_grid: &VoxelGrid<Voxel>, input_params: &I
     sun_positions.par_iter().for_each(|sun_position| {
         let rot_voxel_key_pairs = get_rotated_voxel_key_pair_iterator(voxel_grid, sun_position);
 
-        let mut voxel_illumination_map: HashMap<
-            (i64, i64),
-            (i64, &Voxel),
-            BuildHasherDefault<XxHash64>,
-        > = HashMap::default();
+        let voxel_illumination_map = IlluminationMap::create();
 
         for rot_voxel_key_pair in rot_voxel_key_pairs {
-            let key = {
-                let (x, y, _z) = rot_voxel_key_pair.rotated_key.as_tuple();
-                (x, y)
-            };
-
-            if voxel_illumination_map.get(&key).is_some() {
-                let voxel_in_shadow = {
-                    let (last_rot_voxel_key_pair_z, last_rot_voxel_ref) =
-                        *voxel_illumination_map.get(&key).unwrap();
-
-                    if rot_voxel_key_pair.rotated_key.z < last_rot_voxel_key_pair_z {
-                        voxel_illumination_map.insert(
-                            key,
-                            (
-                                rot_voxel_key_pair.rotated_key.z,
-                                rot_voxel_key_pair.reference,
-                            ),
-                        );
-
-                        last_rot_voxel_ref
-                    } else {
-                        rot_voxel_key_pair.reference
-                    }
-                };
-
+            let rot_voxel_key_pair = Rc::new(rot_voxel_key_pair);
+            if let Some(voxel_in_shadow) =
+                voxel_illumination_map.get_voxel_in_shadow(rot_voxel_key_pair)
+            {
                 let irradiance =
                     get_irradiance(input_params, voxel_in_shadow, sun_position, no_of_day, true);
 
                 update_global_irradiance(voxel_in_shadow, &irradiance, true);
-            } else {
-                voxel_illumination_map.insert(
-                    key,
-                    (
-                        rot_voxel_key_pair.rotated_key.z,
-                        rot_voxel_key_pair.reference,
-                    ),
-                );
             }
         }
 
-        for (_z, illuminated_voxel) in voxel_illumination_map.values() {
+        for (_z, illuminated_voxel) in voxel_illumination_map.borrow_mut().values() {
             let irradiance = get_irradiance(
                 input_params,
                 illuminated_voxel,
--- src/radiation/illumination/structs.rs
+++ src/radiation/illumination/structs.rs
@@ -1,7 +1,64 @@
 use crate::voxel::{Key, Voxel};
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::hash::BuildHasherDefault;
+use std::rc::Rc;
+use twox_hash::XxHash64;
 
 pub struct RotatedVoxelKeyPair<'a> {
     pub reference: &'a Voxel,
     pub rotated_key: Key,
 }
 
+pub type IlluminationMap<'a> =
+    RefCell<HashMap<(i64, i64), (i64, &'a Voxel), BuildHasherDefault<XxHash64>>>;
+
+pub trait IlluminationMapUtils<'a> {
+    fn create() -> IlluminationMap<'a> {
+        RefCell::new(HashMap::default())
+    }
+
+    fn get_voxel_in_shadow(
+        &'a self,
+        rot_voxel_key_pair: Rc<RotatedVoxelKeyPair<'a>>,
+    ) -> Option<&'a Voxel>;
+}
+
+impl<'a> IlluminationMapUtils<'a> for IlluminationMap<'a> {
+    fn get_voxel_in_shadow(
+        &'a self,
+        rot_voxel_key_pair: Rc<RotatedVoxelKeyPair<'a>>,
+    ) -> Option<&'a Voxel> {
+        let key = {
+            let (x, y, _z) = rot_voxel_key_pair.rotated_key.as_tuple();
+            (x, y)
+        };
+
+        if self.borrow().get(&key).is_some() {
+            let (last_rot_voxel_key_pair_z, last_rot_voxel_ref) = *self.borrow().get(&key).unwrap();
+
+            if rot_voxel_key_pair.rotated_key.z < last_rot_voxel_key_pair_z {
+                self.borrow_mut().insert(
+                    key,
+                    (
+                        rot_voxel_key_pair.rotated_key.z,
+                        rot_voxel_key_pair.reference,
+                    ),
+                );
+
+                Some(last_rot_voxel_ref)
+            } else {
+                Some(rot_voxel_key_pair.reference)
+            }
+        } else {
+            self.borrow_mut().insert(
+                key,
+                (
+                    rot_voxel_key_pair.rotated_key.z,
+                    rot_voxel_key_pair.reference,
+                ),
+            );
+            None
+        }
+    }
+}
